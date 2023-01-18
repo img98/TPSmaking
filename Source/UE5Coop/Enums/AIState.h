22dbed35accf04f1c9b809ce87daea368d50d911
img98 <nichy98@naver.com>
2023-01-18 20:57:39 +0900

    feat : ChaseHoldingAttack 적AI 구현중, Attacking BT 구현 필요

--- Source/UE5Coop/Enums/AIState.h
+++ Source/UE5Coop/Enums/AIState.h
@@ -3,9 +3,9 @@
 UENUM(BlueprintType)
 enum class EAIState :uint8
 {
+	EAIS_Chasing UMETA(DisplayName = "Chasing"),
 	EAIS_Holding UMETA(DisplayName = "Holding"),
 	EAIS_Attacking UMETA(DisplayName = "Attacking"),
-	EAIS_Recovery UMETA(DisplayName = "Recovery"),
 
 	EIT_MAX UMETA(DisplayName = "DefaultMAX")
 };
